c3aae0f541ad869b87f293d6369b5e77fee6be1a
hasanussafa <45102174+hasanussafa@users.noreply.github.com>
2023-07-04 14:47:09 -0400

    Add files via upload

---
+++ add2numbers.py
@@ -0,0 +1,7 @@
+a=input(" Please enter first number:")
+b=input(" Please enter second number:")
+if(a>b):
+    c= (int(a)-int(b))
+else:
+    c= (int(b)-int(a))
+print("Differenc4e betwwen two  numbers is ", c)
